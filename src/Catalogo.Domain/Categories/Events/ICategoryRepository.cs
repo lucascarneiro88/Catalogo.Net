4dc83c55ad13865f4403adcdc617a35a123f41c2
Lucas <carneirolucas88@gmail.com>
2025-05-08 13:15:00 -0300

    feat:aadd ICategory interface

---
+++ src/Catalogo.Domain/Categories/Events/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+namespace Catalogo.Domain.Categories;
+
+public interface ICategoryRepository 
+{
+//metodo para devolver todas as categorias
+    Task<List<Category>> GetAll();//task assincrono, para buscar a lista de categorias
+    void Add(Category category);//metodo para adicionar uma categoria
+}
+
+//aqui só implementa o que vai fazer a aplicação, como vai fazer sera implemntado na camada de infra
