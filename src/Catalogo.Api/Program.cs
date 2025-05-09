8bca9158550bc18582291825f729cf8201550573
Lucas <carneirolucas88@gmail.com>
2025-05-09 16:58:51 -0300

    feat: integrate infrastructure services into application startup

--- src/Catalogo.Api/Program.cs
+++ src/Catalogo.Api/Program.cs
@@ -1,10 +1,14 @@
+using Catalogo.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddInfrastructure(builder.Configuration);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
