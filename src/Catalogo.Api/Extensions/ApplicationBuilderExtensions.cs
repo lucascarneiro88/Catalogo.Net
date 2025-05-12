e23de2eb6fda3a0128cd37bc00d8644937a1a938
Lucas <carneirolucas88@gmail.com>
2025-05-12 13:25:57 -0300

    feat: create extension method for applying migrations

---
+++ src/Catalogo.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Catalogo.Domain.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.Api.Extensins;
+
+public static class ApplicationBuilderExtensions
+{
+    public static async Task ApplyMigration(this IApplicationBuilder app)// metodo de extensao
+    {
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+            var service = scope.ServiceProvider;// cria um escopo para o serviço
+            var loggerFactory = service.GetRequiredService<ILoggerFactory>();// cria um logger para o serviço
+
+
+            try
+            {
+                var context = service.GetRequiredService<CatalogoDbContext>();// cria um contexto para o banco de dados
+                await context.Database.MigrateAsync();
+            }
+            catch (Exception ex)// captura a exceção
+            {
+                var logger = loggerFactory.CreateLogger<Program>();// cria um logger para o programa
+                logger.LogError(ex, "Error in apply migration");// loga o erro
+            }
+
+        }
+    }
+}
