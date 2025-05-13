44a30208e8aedec2e0d3937409df27e19b73a300
Lucas <carneirolucas88@gmail.com>
2025-05-13 15:33:03 -0300

    feat: add GetIdAsync method to IProductRepository

--- src/Catalogo.Domain/Products/Events/IProductRepository.cs
+++ src/Catalogo.Domain/Products/Events/IProductRepository.cs
@@ -2,9 +2,22 @@ using Catologo.Domain.Products;
 
 namespace Catalogo.Domain.Products;
 
+// Interface que define o contrato para repositório de produtos
+// Ela obriga qualquer classe que a implemente a fornecer essas funcionalidades
 public interface IProductRepository
 {
-    //metodo para buscar produto pelo codigo(id, codigo de barras, sku,  etc)
+    // Método assíncrono que busca um produto com base em um código único
+    // Pode ser ID, código de barras, SKU, etc.
     Task<Product> GetByCode(string code);
-    Task<List<Product>> GetAll();//metodo para buscar lista de todos produtos
+
+    // Método assíncrono que retorna uma lista com todos os produtos cadastrados
+    Task<List<Product>> GetAll();
+
+    // Método para adicionar um novo produto ao repositório
+    // Ainda não salva no banco — isso será feito no SaveChangesAsync()
+    void Add(Product product);
+
+    // Método assíncrono que busca um produto pelo seu Id (Guid)
+    // Recebe um CancellationToken para permitir cancelamento da operação, se necessário
+    Task<Product> GetIdAsync(Guid id, CancellationToken cancellationToken);
 }
